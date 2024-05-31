3c2a6a3d7ff64554b3c6f58f579bae7d838eef4b
0940185778 <biniyamassefa648@gmail.com>
2024-05-31 21:59:16 +0300

    customer profile edited

--- skill_trade/lib/presentation/widgets/customer_profile.dart
+++ skill_trade/lib/presentation/widgets/customer_profile.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skill_trade/models/customer.dart';
 import 'package:skill_trade/presentation/widgets/info_label.dart';
-import 'package:skill_trade/riverpod/customer_provider.dart';
-
-
 
 class customerProfile extends StatelessWidget {
   final Customer customer;
@@ -13,25 +9,23 @@ class customerProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(top: 60),
-      child: Center( 
-        child:Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage("assets/profile.jpg"),
-                ),
-                SizedBox(height: 10),
-                InfoLabel(label: "Name", data: customer.fullName,),
-                InfoLabel(label: "Email", data: customer.email),
-                InfoLabel(label: "Phone", data: customer.phone),
-              ],
-            )
-
-      )
-            
-        
-    );
+        padding: const EdgeInsets.only(top: 60),
+        child: Center(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage("assets/profile.jpg"),
+            ),
+            const SizedBox(height: 10),
+            InfoLabel(
+              label: "Name",
+              data: customer.fullName,
+            ),
+            InfoLabel(label: "Email", data: customer.email),
+            InfoLabel(label: "Phone", data: customer.phone),
+          ],
+        )));
   }
 }
