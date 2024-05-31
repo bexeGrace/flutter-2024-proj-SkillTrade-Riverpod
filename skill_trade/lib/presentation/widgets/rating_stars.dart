30eb6628634c55b6d8479032653ea6b257f3dbd4
0940185778 <biniyamassefa648@gmail.com>
2024-05-31 22:09:14 +0300

    rating edited

--- skill_trade/lib/presentation/widgets/rating_stars.dart
+++ skill_trade/lib/presentation/widgets/rating_stars.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 
 class RatingStars extends StatelessWidget {
   final int rating;
-  RatingStars({super.key, required this.rating});
+  const RatingStars({super.key, required this.rating});
 
   @override
   Widget build(BuildContext context) {
     return Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Icon(Icons.star, color: rating >= 1 ? Colors.orange : Colors.grey),
-          Icon(Icons.star, color: rating >= 2 ? Colors.orange : Colors.grey),
-          Icon(Icons.star, color: rating >= 3 ? Colors.orange : Colors.grey),
-          Icon(Icons.star, color: rating >= 4 ? Colors.orange : Colors.grey),
-          Icon(Icons.star, color: rating >= 5 ? Colors.orange : Colors.grey),
-        ],
-      );
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Icon(Icons.star, color: rating >= 1 ? Colors.orange : Colors.grey),
+        Icon(Icons.star, color: rating >= 2 ? Colors.orange : Colors.grey),
+        Icon(Icons.star, color: rating >= 3 ? Colors.orange : Colors.grey),
+        Icon(Icons.star, color: rating >= 4 ? Colors.orange : Colors.grey),
+        Icon(Icons.star, color: rating >= 5 ? Colors.orange : Colors.grey),
+      ],
+    );
   }
 }
