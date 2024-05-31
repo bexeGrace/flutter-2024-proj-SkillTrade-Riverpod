4679bca866d4a95749999fb65417616deb795d3e
kalebish <phy.rules.kab@gmail.com>
2024-05-31 23:31:37 +0300

    final changes on riverpod tests

--- skill_trade/integration_test/technician_signup_test.dart
+++ skill_trade/integration_test/technician_signup_test.dart
@@ -45,27 +45,23 @@ void main() async {
       await tester.tap(technicianAvatarFinder);
       await tester.pumpAndSettle();
 
-      // Enter full name
       await tester.enterText(find.bySemanticsLabel('Fullname'), 'Jane Smith');
-      // Enter email
       await tester.enterText(
           find.bySemanticsLabel('email'), 'jane.smith@example.com');
-      // Enter phone
       await tester.enterText(find.bySemanticsLabel('phone'), '0987654321');
-      // Enter password
+
       await tester.enterText(find.bySemanticsLabel('password'), 'password123');
-      // Enter experience
       await tester.enterText(find.bySemanticsLabel('Experience'), '5 years');
-      // Enter education level
+
       await tester.enterText(
           find.bySemanticsLabel('Education Level'), 'Bachelor\'s Degree');
-      // Enter location
+
       await tester.enterText(
           find.bySemanticsLabel('Available location'), 'New York');
-      // Enter bio
+
       await tester.enterText(find.bySemanticsLabel('Additional Bio'),
           'Experienced technician with expertise in HVAC.');
-      // Select skills
+
       final skill1 = find.text('Electricity');
       final skill2 = find.text('Plumbing');
 
@@ -77,22 +73,17 @@ void main() async {
       await tester.tap(skill2);
       await tester.pumpAndSettle();
 
-      // Dismiss the keyboard
       await tester.tapAt(Offset(0, 0));
       await tester.pumpAndSettle();
 
-      // Scroll the signup button into view
       final signupButtonFinderForm = find.text('Apply');
       await tester.ensureVisible(signupButtonFinderForm);
       await tester.pumpAndSettle();
 
-      // Tap the signup button
       await tester.tap(signupButtonFinderForm);
 
-      // Wait for backend response and app to settle
       await tester.pumpAndSettle();
 
-      // Verify that TechnicianPage is displayed after signup
       expect(await find.byType(TechnicianApplication), findsOneWidget);
     });
   });
@@ -101,27 +92,20 @@ void main() async {
       await tester.pumpWidget(MyApp());
       await tester.pumpAndSettle();
 
-      // Wait for the app to settle
       await tester.pumpAndSettle();
 
-      // Verify that HomeScreen is displayed
       expect(find.byType(HomeScreen), findsOneWidget);
       expect(find.text('SkillTrade Hub'), findsOneWidget);
 
-      // Tap the login button
       final loginButtonFinder = find.text('Login');
       await tester.ensureVisible(loginButtonFinder);
       await tester.pumpAndSettle();
       await tester.tap(loginButtonFinder);
       await tester.pumpAndSettle();
 
-      // Verify that LoginPage is displayed
       expect(find.byType(LoginPage), findsOneWidget);
 
-      // Enter email
-      await tester.enterText(
-          find.bySemanticsLabel('email'), 'jane.smith@example.com');
-      // Enter password
+      await tester.enterText( find.bySemanticsLabel('email'), 'jane.smith@example.com');
       await tester.enterText(find.bySemanticsLabel('password'), 'password123');
       await tester.pumpAndSettle();
 
@@ -129,37 +113,29 @@ void main() async {
       await tester.tap(technicianRadioFinder);
       await tester.pumpAndSettle();
 
-      // Scroll the login button into view
       final loginButtonFinderForm = find.text('login');
       await tester.ensureVisible(loginButtonFinderForm);
       await tester.pumpAndSettle();
 
-      // Tap the login button
+ 
       await tester.tap(loginButtonFinderForm);
 
-      // Wait for backend response and app to settle
       await tester.pumpAndSettle();
 
-      // Verify that TechnicianPage is displayed after login
       expect(await find.byType(TechnicianPage), findsOneWidget);
 
-
-
-      // Tap to open the drawer
       final openDrawerIcon = find.byIcon(Icons.menu); 
       await tester.ensureVisible(openDrawerIcon);
-      // Assuming the menu icon opens the drawer
+
       await tester.tap(openDrawerIcon);
 
       await tester.pumpAndSettle();
 
-      // Tap the logout option
       final logoutOptionFinder = find.text('Logout');
       await tester.ensureVisible(logoutOptionFinder);
       await tester.tap(logoutOptionFinder);
       await tester.pumpAndSettle();
 
-      // Verify that HomeScreen is displayed again after logout
       expect(await find.byType(HomeScreen), findsOneWidget);
     });
   }, skip: "skip for now");
